12b81e19aac995be7b4c1e89f15908aae315ea34
brenoxdmoon <brenogabriel19@gmail.com>
2024-09-01 12:40:49 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/brenoxdmoon/chopper_house_kotlin/ChopperHouseKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.brenoxdmoon.chopper_house_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChopperHouseKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChopperHouseKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/brenoxdmoon/chopper_house_kotlin/ChopperHouseKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.chopper_house_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChopperHouseKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
